f1bcf83b3a6357407c390c208ed7e37542f3ff32
wintermute-ui <timonmartell02@gmail.com>
2023-11-25 17:23:17 -0800

    Added better command-line functionality

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from typing import List, Set, Tuple
+import sys,argparse
 """
 GAME RULES (according to NYT)
 ------------------------------------------------------------------
@@ -38,7 +39,7 @@ def filter_words(filename: str) -> None:
         for word in word_list:
             f.write(word.lower() + "\n")
 
-def prune_words(sigma: List[str], accept_ls: Set[str], sides: List[set]) -> List[str]:
+def prune_words(sigma: List[str], accept_ls: Set[str], sides: List[set], exclude:List[str]=[]) -> List[str]:
 
     # Get compliment of set of letters
     comple_letters = sigma
@@ -48,10 +49,12 @@ def prune_words(sigma: List[str], accept_ls: Set[str], sides: List[set]) -> List
         
     # Get list of accepted words according to letters allowed
     new_list = []
-        
+
     with open("words.txt", "r") as f:
-        for word in f:
+        for word in f:  
             w = word.strip()
+            if w in exclude:
+                continue
             not_in = False
             for letter in comple_letters:
                 if letter in w:
@@ -374,58 +377,104 @@ def print_all_solutions(accepted_words: List[str], accept_ls: Set[str]) -> None:
             solved_orders.append(solved_order)
 
         max_iterations+=1
-        
 
-if __name__ == "__main__":
-    # filter_words("Collins_Scrabble_Words.txt")
-        
-    # Get User Input (Game Layout)
-    reset = 0
-    while not reset:
-        ls11 = input("Letter 1 of side 1: ").strip().lower()
-        ls21 = input("Letter 2 of side 1: ").strip().lower()
-        ls31 = input("Letter 3 of side 1: ").strip().lower()
-        reset = 1
-        #reset = input(f"Side 1 = {[ls11, ls21, ls31]} [y/n] ") != "n"
-    print("")
+def a_solution_in_n(accepted_words: List[str], accept_ls: Set[str], n_words:int) -> None:
 
-    reset = 0
-    while not reset:
-        ls12 = input("Letter 1 of side 2: ").strip().lower()
-        ls22 = input("Letter 2 of side 2: ").strip().lower()
-        ls32 = input("Letter 3 of side 2: ").strip().lower()
-        reset = 1
-        #reset = input(f"Side 2 = {[ls12, ls22, ls32]} [y/n] ") != "n"
-    print("")
+    # Sort accepted words into dict where the starting character is the key to values of class letterboxd_word (dict(letter:[word1,word2,...]))
+    letter_dict = {letter:[letterboxd_word(word) for word in accepted_words if word[0] == letter] for letter in accept_ls}
 
-    reset = 0
-    while not reset:
-        ls13 = input("Letter 1 of side 3: ").strip().lower()
-        ls23 = input("Letter 2 of side 3: ").strip().lower()
-        ls33 = input("Letter 3 of side 3: ").strip().lower()
-        reset = 1
-        #reset = input(f"Side 3 = {[ls13, ls23, ls33]} [y/n] ") != "n"
-    print("")
+    for key in letter_dict:
+        letter_dict[key].sort(reverse=True)
 
-    reset = 0
-    while not reset:
-        ls14 = input("Letter 1 of side 4: ").strip().lower()
-        ls24 = input("Letter 2 of side 4: ").strip().lower()
-        ls34 = input("Letter 3 of side 4: ").strip().lower()
-        reset = 1
-        #reset = input(f"Side 4 = {[ls14, ls24, ls34]} [y/n] ") != "n"
-    print("")
+    # Log past first guesses and solved orders to compare
+    completed_letters = set()
+    first_guesses = []
+    solved_orders = []
 
+    max_iterations = 0
+
+    # Repeat Until Solution
+    while (max_iterations < len(accepted_words)): 
+        # Start on the word with the greatest score
+        best_guess = letterboxd_word("")
+        for key in letter_dict:
+            for word in letter_dict[key]:
+                if word in first_guesses:
+                    continue
+                if best_guess.score > word.score:
+                    continue
+                else:
+                    best_guess = word
+
+        completed_letters = set()
+        solved_order = []
+        count = 0
+
+        start_letter = best_guess.word[0]
+        # add best_guess to first_guesses as to not reuse when retrying
+        first_guesses.append(best_guess)
+        completed_letters.update(best_guess.letters)
+        solved_order.append(best_guess)
+
+        while completed_letters != accept_ls and count < 3:
+            #print(completed_letters, ":", accept_ls, completed_letters==accept_ls)
+
+            start_letter = best_guess.word[-1]
+            if letter_dict[start_letter]:
+                best_guess = letter_dict[start_letter][0]
+            else:
+                break
+
+            for word in letter_dict[start_letter]:
+                if len(best_guess.letters - completed_letters) < len(word.letters - completed_letters):
+                    best_guess = word
+
+            #letter_dict[start_letter].remove(best_guess)
+            completed_letters.update(best_guess.letters)
+            solved_order.append(best_guess)
+
+            count += 1
+
+        if completed_letters == accept_ls:
+            if len(solved_order) <= n_words:
+                print(tuple(solved_order))
+                solved_orders.append(solved_order)
+
+        max_iterations+=1
     
-    s1 = set([ls11, ls21, ls31])
-    s2 = set([ls12, ls22, ls32])
-    s3 = set([ls13, ls23, ls33])
-    s4 = set([ls14, ls24, ls34])
+    if len(solved_orders) == 0:
+        print(f"No Solution Found in {n_words} words!")
 
-    accept_letters = set([ls11, ls21, ls31, ls12, ls22, ls32, ls13, ls23, ls33, ls14, ls24, ls34])
+def solve(letterbox_set: List[List[str]] = [], p: bool=False, n:int=0, exclude:List[str]=[]):
+    """
+    letterbox_set: List of the sides of a letterboxed puzzle (format: [['a','b','c], ['d','e','f], ...])
+    """
+    if letterbox_set == []:
 
-    sides = [s1,s2,s3,s4]
-    # print(sides)
+        # Get User Input (Game Layout, Format: s1 = "abc", s2 = "def", ...)
+        ls1 = input("Letters of Side 1: ").strip()
+        ls2 = input("Letters of Side 2: ").strip()
+        ls3 = input("Letters of Side 3: ").strip()
+        ls4 = input("Letters of Side 4: ").strip()
+        
+        s2 = [l.lower() for l in ls2]
+        s1 = [l.lower() for l in ls1]
+        s3 = [l.lower() for l in ls3]
+        s4 = [l.lower() for l in ls4]
+
+    elif len(letterbox_set) >= 4:
+        s1 = letterbox_set[0]
+        s2 = letterbox_set[1]
+        s3 = letterbox_set[2]
+        s4 = letterbox_set[3]
+
+    else:
+        print("Invaild Input!")
+        exit(1)
+    
+    accept_letters = set(s1+s2+s3+s4)
+
+    sides = [set(s1),set(s2),set(s3),set(s4)]
 
     # Check input follows game setup rules
     if len(accept_letters) != 12:
@@ -443,16 +492,54 @@ if __name__ == "__main__":
 
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    prune_list = prune_words(alpha, accept_letters, sides)
+    prune_list = prune_words(alpha, accept_letters, sides, exclude)
+
+    if not p:
+        if n > 0:
+            solution_list = compile_solution_in_n(prune_list, accept_letters, n)
+        else:
+            solution_list = compile_all_solutions(prune_list, accept_letters)
+
+        return solution_list
     
-    # solution_list = compile_all_solutions(prune_list, accept_letters)
+    else:
+        if n > 0:
+            print_solution_in_n(prune_list, accept_letters, n)
+        else:
+            print_all_solutions(prune_list, accept_letters)
 
-    # solution_list.sort(key=lambda s: len(s))
+class CommandLine:
+    def __init__(self):
+        parser = argparse.ArgumentParser(description = "Solves a LetterBoxed square")
+        parser.add_argument("side", metavar="S", help="The letters on a side of a Letterboxed square", nargs='*', type=str, default="")
+        parser.add_argument("-n", "--num", help = "Example: Print argument", required = False, type=int, default = 0)
+        parser.add_argument("-p", "--print", help = "Example: Print argument", action="store_true", required = False, default = "")
+        parser.add_argument("-e", "--exclude", help = "Words excluded from master word list", nargs="+", type=str, default = "")
 
-    # print(solution_list)
+        argument = parser.parse_args()
+        sides = []
+        exclusion = []
 
-    # print(prune_list, len(prune_list))
-
-    print_all_solutions(prune_list, accept_letters)
+        if argument.side:
+            for s in argument.side:
+                sides.append([l.lower() for l in s])
         
-    
+        if argument.exclude:
+            exclusion = [ex.strip().lower() for ex in argument.exclude]
+
+        solve(sides, argument.print, argument.num, exclusion)
+        
+        
+
+if __name__ == "__main__":
+    app = CommandLine()
+
+    # if len(sys.argv) >= 6:
+    #     s = solve([[l.lower() for l in sys.argv[1]], [l.lower() for l in sys.argv[2]], [l.lower() for l in sys.argv[3]], [l.lower() for l in sys.argv[4]]], int(sys.argv[5]))
+    # elif len(sys.argv) >= 5:
+    #     s = solve([[l.lower() for l in sys.argv[1]], [l.lower() for l in sys.argv[2]], [l.lower() for l in sys.argv[3]], [l.lower() for l in sys.argv[4]]])
+    # else:
+    #     s = solve()
+
+    # for sol in s: 
+    #     print(sol)
